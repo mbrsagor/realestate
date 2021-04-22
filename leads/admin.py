058d7ed5564323075e1eba2101af246522b191c2
mbrsagor <mbrsagor@gmail.com>
2021-04-22 14:31:31 +0600

    Member models created

--- leads/admin.py
+++ leads/admin.py
@@ -4,18 +4,18 @@ from leads.models.book import Book
 from leads.models.lead import Lead
 
 
-class LeadManager(admin.ModelManager):
-    list_display = ['id', 'title', 'author']
+class LeadManager(admin.ModelAdmin):
+    # list_display = ['id', 'title', 'author']
     search_fields = ['id', 'title']
-    list_editable = ['title']
+    # list_editable = ['title', ]
     list_per_page = 8
        
 admin.site.register(Lead, LeadManager)
 
-class BookManager(admin.ModelManager):
-    list_display = ['id', 'name', 'author', 'email', 'comments', 'messages']
+class BookManager(admin.ModelAdmin):
+    # list_display = ['id', 'name', 'author', 'email', 'comments', 'messages']
     search_fields = ['email', 'name']
-    list_editable = ['name', 'email']
+    # list_editable = ['name', 'email']
     list_per_page = 8
 
-admin.site.register(Book)
+admin.site.register(Book, BookManager)
