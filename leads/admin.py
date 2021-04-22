1e16dfae99d717839cdbff5e3b7a1d27195ae22d
mbrsagor <mbrsagor@gmail.com>
2021-04-22 13:57:12 +0600

    Admin manager added

--- leads/admin.py
+++ leads/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from leads.models.book import Book
 from leads.models.lead import Lead
 
-admin.site.register(Lead)
+
+class LeadManager(admin.ModelManager):
+    list_display = ['id', 'title', 'author']
+    search_fields = ['id', 'title']
+    list_editable = ['title']
+    list_per_page = 8
+       
+admin.site.register(Lead, LeadManager)
+
+class BookManager(admin.ModelManager):
+    list_display = ['id', 'name', 'author', 'email', 'comments', 'messages']
+    search_fields = ['email', 'name']
+    list_editable = ['name', 'email']
+    list_per_page = 8
+
 admin.site.register(Book)
