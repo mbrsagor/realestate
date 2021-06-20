b4c2da2a8fb38fe451d12de670ea946c9b355634
mbrsagor <mbrsagor@gmail.com>
2021-06-20 23:19:34 +0600

    New: User information added django admin

--- lead_auth/admin.py
+++ lead_auth/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import LeadUser
 
-# Register your models here.
+
+class LeadUserAdmin(UserAdmin):
+    list_filter = ('is_active',)
+    list_display = ('email', 'phone_number', 'date_joined', 'last_login', 'is_active')
+    search_fields = ('email', 'phone_number',)
+    readonly_fields = ('date_joined', 'last_login')
+
+
+# admin.site.unregister(LeadUser)
+admin.site.register(LeadUser, LeadUserAdmin)
--- lead_auth/models.py
+++ lead_auth/models.py
@@ -1,14 +1,9 @@
-from datetime import date
 from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import Group
 from django.db import models
-from django.utils import timezone
-import os
-from uuid import uuid4
 
 
 class CustomUserManager(BaseUserManager):
@@ -44,17 +39,52 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserCategory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, null=True, blank=True)
+    category_role = models.ManyToManyField(Group, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
 class LeadUser(AbstractUser):
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
     phone_number = models.CharField(verbose_name='Phone Number', max_length=11, unique=True)
     fullname = models.CharField(max_length=100, blank=True)
+    designation = models.ForeignKey(Position, null=True, blank=True, on_delete=models.CASCADE)
+    user_category = models.ForeignKey(UserCategory, null=True, blank=True, on_delete=models.CASCADE)
     user_note = models.TextField(verbose_name='user Note', blank=True, )
     is_supportUser = models.BooleanField(default=False)
 
-    # USERNAME_FIELD = 'employee_id'
     REQUIRED_FIELDS = ['email', 'phone_number']
 
     objects = CustomUserManager()
 
     def __str__(self):
         return str(self.username)
+
+
+class VerifiedPhone(models.Model):
+    phone_number = models.CharField(max_length=14)
+    otp_code = models.CharField(max_length=6)
+    tracking_id = models.CharField(max_length=50)
+    otp_verify = models.BooleanField(default=False)
+    expire_time = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    next_available_try = models.IntegerField(default=3, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['phone_number', 'tracking_id']),
+        ]
+
+    def __str__(self):
+        return f"{self.pk, self.phone_number}"
