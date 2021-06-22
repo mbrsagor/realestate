3ecafaebe6d40fd9869d4e85419037968ab60d9f
mbrsagor <mbrsagor@gmail.com>
2021-06-23 00:14:29 +0600

    Bug: Positon validation resolved

--- services/validation_service.py
+++ services/validation_service.py
@@ -1,5 +1,7 @@
 def validate_position_data(attrs):
-    if "name" in attrs and len(attrs.get("name")) < 1:
-        return "name field is required"
+    if "name" in attrs and len(attrs.get("name")) <= 1:
+        return "Name field is required"
+    elif "name" in attrs and len(attrs.get("name")) >= 100:
+        return "Name max length not more than 100"
     else:
-        return "Please provide valid name"
+        return None
