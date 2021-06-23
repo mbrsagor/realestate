e63ff4a24b30190c0d1d684ec024c52957285a0b
mbrsagor <mbrsagor@gmail.com>
2021-06-24 00:14:36 +0600

    Refactor: Remove phone OTP

--- lead_auth/models.py
+++ lead_auth/models.py
@@ -88,3 +88,18 @@ class VerifiedPhone(models.Model):
 
     def __str__(self):
         return f"{self.pk, self.phone_number}"
+
+#
+# class PhoneOTP(models.Model):
+#     username = models.CharField(max_length=254, unique=True, blank=True, default=False)
+#     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,14}$',
+#                                  message="Phone number must be entered in the form of +919999999999.")
+#     name = models.CharField(max_length=254, blank=True, null=True)
+#     phone = models.CharField(validators=[phone_regex], max_length=17)
+#     otp = models.CharField(max_length=9, blank=True, null=True)
+#     count = models.IntegerField(default=0, help_text='Number of opt_sent')
+#     validated = models.BooleanField(default=False,
+#                                     help_text='if it is true, that means user have validate opt correctly in seconds')
+#
+#     def __str__(self):
+#         return str(self.phone) + ' is sent ' + str(self.otp)
