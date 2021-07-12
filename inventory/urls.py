44d9ba3917c6451d7d3b52bf18700babc46eb1c2
mbrsagor <mbrsagor@gmail.com>
2021-07-12 08:57:46 +0600

    New: Category updated validation done

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from inventory.views.category_view import CategoryAPIView
 
 urlpatterns = [
+    # Category API endpoint
     path('category/', CategoryAPIView.as_view(), name='category_list'),
     path('category/create/', CategoryAPIView.as_view(), name='category_create'),
+    path('category/update/<pk>/', CategoryAPIView.as_view(), name='category_update'),
     path('category/delete/<pk>/', CategoryAPIView.as_view(), name='category_delete'),
 ]
--- inventory/views/category_view.py
+++ inventory/views/category_view.py
@@ -32,7 +32,18 @@ class CategoryAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk, format=None):
-        pass
+        validation_error = validation_category_data(request.data)
+        if validation_error is not None:
+            return Response(prepare_error_response(validation_error), status=status.HTTP_400_BAD_REQUEST)
+        category = Category.objects.filter(id=pk).first()
+        if category is not None:
+            serializer = CategorySerializer(category, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(prepare_create_success_response(serializer.data), status=status.HTTP_201_CREATED)
+            return Response(prepare_error_response(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(prepare_error_response("No category found for this ID"), status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         category = self.get_object(pk)
